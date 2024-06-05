13fe8c40d2357a954bcca04b32d59cff1624d123
Alexander Droste <alexander.droste@protonmail.com>
2024-06-05 09:10:10 +0200

    clean: set visibility on stack

--- src/main.rs
+++ src/main.rs
@@ -196,21 +196,21 @@ impl ViewTree {
                                 *state += 1;
                                 println!("clicked {}", state);
                             }
-                        })),
+                        }))
                 ))
             ))
             .spacing(40.0),
 
             HStack::new((
-                Loop::new(0..10, |idx|
+                Loop::new(0..18, |idx|
                     VStack::new((
                         Loop::new(0..10, |idx2|
                             Circle::default()
                                 .stroke(Color::rgba8(122, 122, 255, 50), 2.0)
                                 .fill(Color::rgb8(25 * idx2 as u8, 25 * idx2 as u8, 25 * idx2 as u8))
-                                .diameter(10.0 * (idx + 1) as f64 / 2.0)
-                                .visible(idx % 2 == 0)
+                                .diameter(5.0 * (idx + 1) as f64 / 2.0)
                     )))
+                    .visible(idx % 2 == 0)
                     .spacing(20.0)
                 )
             ))
